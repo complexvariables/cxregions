a82abc4fcc5b01c6699bca154a5addade882841e
Toby Driscoll <driscoll@udel.edu>
2026-01-12 22:28:46 -0500

    version bump to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cxregions",
-    version="0.1.2", 
+    version="0.1.4", 
     author="Toby Driscoll", 
     author_email="driscoll@udel.edu",  
     description="A Python interface to the ComplexRegions.jl Julia package",
